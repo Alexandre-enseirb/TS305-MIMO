a7d5722981837c5f46dd7ba9fbb86518c4fe9ef7
Adrial-Knight <pierreminier64@gmail.com>
2022-12-04 13:19:38 +0100

    [VBLAST ML] optimisation cout
    Retire la racine carrée inutile (fonction strictement croissante).

--- functions/VBLAST_decode_ML.m
+++ functions/VBLAST_decode_ML.m
@@ -4,7 +4,7 @@ function X_hat = VBLAST_decode_ML(Y, H, C)
     X_hat = zeros(N, L);  % estimation des symboles émis
     for i_y = 1:L
         symbole_rec = Y(:, i_y);  % les symboles reçus sur les M antennes
-        cout = sqrt(sum((abs(symbole_rec - H*C)).^2));
+        cout = sum((abs(symbole_rec - H*C)).^2);
         [~, ind_min] = min(cout);
         X_hat(:, i_y) = C(:, ind_min);
     end
