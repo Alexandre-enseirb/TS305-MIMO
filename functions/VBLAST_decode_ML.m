74027a9482bf887b69dd8f1566e64e92123b7fa4
Adrial-Knight <pierreminier64@gmail.com>
2022-12-07 20:11:01 +0100

    [VBLAST ML]
    Erreur lors de la définition de la longueur de cohérence du canal L

--- functions/VBLAST_decode_ML.m
+++ functions/VBLAST_decode_ML.m
@@ -1,6 +1,6 @@
 function X_hat = VBLAST_decode_ML(Y, H, C, ~)
     N = size(C, 1);  % nb d'antennes à l'émission
-    L = size(H, 2);  % nb de symboles pour lesquels H est cste
+    L = size(Y, 2);  % nb de symboles pour lesquels H est cste
     X_hat = zeros(N, L);  % estimation des symboles émis
     for i_y = 1:L
         symbole_rec = Y(:, i_y);  % les symboles reçus sur les M antennes
